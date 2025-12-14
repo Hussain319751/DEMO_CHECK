04585d5d61e9c2635750897d390890ad02c45c8b
Hussain319751 <hussain319751@gmail.com>
2025-12-14 10:25:26 +0530

    Add files via upload

---
+++ strings_osModule.py
@@ -0,0 +1,51 @@
+# print("first_prac")
+
+
+#========IT WILL SPEAK THE TEST=======
+
+# import pyttsx3
+# speaker=pyttsx3.init()
+# speaker.say("hi ela unnaru meeru i hope bagunnaru kada")
+# speaker.runAndWait()
+
+
+#===========PRINT THE DIRECTOREIS OF OUR SYSTEM BASED ON THE PATH GIVEN======
+
+
+
+# import os
+# directories="/springNotes/oops"
+# list_dir=os.listdir(directories)
+# for dire in list_dir:
+#   print(dire)
+
+
+#===========STRING  OPERTAIONS=========
+
+
+
+name="   chinna is  god  boy     "
+print(name[-5:-2])
+
+print(len(name))
+
+print(name.capitalize())
+print(name .title())
+print(name.startswith("chinna"))
+print(name.count("i"))
+print(name.index("i"))
+print(name.split(' '))
+str_ar=name.split(' ')
+# for str in str_ar:
+#   print(str)
+print(name[0:6:2])
+print(name.strip())
+print(name.lstrip()) 
+print(name.replace(" ",""))
+
+
+user_name=input("enter your name")
+print(f"gd afternoon {user_name}")
+
+print(name.index("g"))
+
